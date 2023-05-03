88deef446ad4b8e34e7d9dfdd4a29c6e15651338
Anshuman Bhardwaj <64587347+A-nshuman@users.noreply.github.com>
2023-05-04 00:13:46 +0300

    Add files via upload

---
+++ dice_random.py
@@ -0,0 +1,23 @@
+import random
+from time import sleep
+
+def roll_dice():
+    print("Dice Rolling ... ")
+    sleep(0.5)
+    num = random.randint(1,6)
+    print(f"You got {num}")
+
+while True:
+    print("1. Roll Dice\n2. Exit")
+    choice = int(input("Enter your choice : "))
+
+    if choice == 1:
+        roll_dice()
+
+    elif choice == 2:
+        print("GoodNight ( ͡~ ͜ʖ ͡° )")
+        break
+
+    else:
+        print("Wrong Choice, Try Again")
+        continue
