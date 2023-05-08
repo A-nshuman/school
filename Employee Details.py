28b532501b1c541ebcc17936f9535acb6075e2a5
Anshuman Bhardwaj <64587347+A-nshuman@users.noreply.github.com>
2023-05-08 23:57:02 +0300

    Update Employee Details.py

--- Employee Details.py
+++ Employee Details.py
@@ -25,9 +25,12 @@ def disp_emps():
     for i in r_obj:
         tab.add_row([f"{i[0]}", f"{i[1]}", f"{i[2]}", f"{i[3]}"])
     print(tab)
+    # print("ID\t\tName\t\tAddress\t\tSalary")
+    # for i in r_obj:
+    #     print(i[0], '\t\t',i[1], '\t\t',i[2], '\t\t',i[3])
     f.close()
 
-def app_emp():
+def add_emp():
     f = open("School/emp_det.csv", "a", newline='')
     emps = []
     while True:
@@ -57,20 +60,32 @@ def search_emp():
         else:
             print("Employee Data does not exist")
 
+def del_emp():
+    ID = int(input("Enter employee ID : "))
+    with open("School/emp_det.csv", "r") as file:
+        r_obj = csv.reader(file)
+        nlis = list(r_obj)
+        for i in nlis:
+            if int(i[0]) == ID:
+                global ind
+                ind = nlis.index(i)
+        del nlis[ind]
+
+    with open("School/emp_det.csv", "w", newline='') as file:
+        w_obj = csv.writer(file)
+        w_obj.writerows(nlis)
+
 while True:
     print("Employee Database")
-    print("1. Create New Database \n2. Insert Employee Details \n3. Search Employee Details \n4. Display All Data \n5. EXIT")
+    print("1. Create New Database \n2. Insert Employee Details \n3. Search Employee Details \n4. Display All Data \n5. Delete Employee Details \n6. EXIT")
     ch = int(input("Enter your choice : "))
     
-    if ch == 1:
-        write_emp()
-    elif ch == 2:
-        app_emp()
-    elif ch == 3:
-        search_emp()
-    elif ch == 4:
-        disp_emps()
-    elif ch == 5:
+    if ch == 1: write_emp()
+    elif ch == 2: add_emp()
+    elif ch == 3: search_emp()
+    elif ch == 4: disp_emps()
+    elif ch == 5: del_emp()
+    elif ch == 6:
         print("Exited Employee Database")
         break
     else:
