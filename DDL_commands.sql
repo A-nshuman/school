d90a90da985042992cba8a6b237fc4023421b027
Anshuman Bhardwaj <64587347+A-nshuman@users.noreply.github.com>
2023-10-15 23:14:41 +0300

    Create DDL_commands.sql

---
+++ DDL_commands.sql
@@ -0,0 +1,9 @@
+use school;
+create table sinfo(roll int(4), name varchar(225), stream varchar(225));
+alter table sinfo add address varchar(225);
+alter table sinfo drop column address;
+alter table sinfo modify column name varchar(30);
+alter table sinfo add primary key (roll);
+desc sinfo;
+create table sinfo1(roll_no int(4) primary key, class int(2), foreign key (roll_no) references sinfo(roll));
+desc snifo1;
